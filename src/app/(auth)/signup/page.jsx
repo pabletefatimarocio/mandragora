87a37a924d3c527be838450dc52e6183047a627d
pabletefatimarocio <pabletefatima9@gmail.com>
2025-05-15 16:59:32 -0500

    C:/Program Files/Git/signup styles

---
+++ src/app/(auth)/signup/page.jsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import styles from "./signup.module.css";
+import { FcGoogle } from "react-icons/fc";
+
+export default function Signup() {
+  return (
+    <div className={styles.container}>
+      <form className={styles.form}>
+        <input type="text" placeholder="Email"></input>
+        <input type="password" placeholder="Contraseña"></input>
+        <input type="password" placeholder="Confirmar contraseña"></input>
+        <div>
+          <button>Continuar</button>
+          <span >
+            Ya tienes una cuenta? <Link href="/login">Ingresar</Link>
+          </span>
+        </div>
+      </form>
+      <div className={styles.o}> 
+      <span>o</span>
+      </div>
+      <Link href="/signup" className={styles.googleBtn}>
+        <FcGoogle fontSize='30px'/> Google
+      </Link>
+    </div>
+  );
+}
