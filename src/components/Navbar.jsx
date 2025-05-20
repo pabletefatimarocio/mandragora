fc2869157a0f4928186cacf40d8e143b188b0b27
pabletefatimarocio <pabletefatima9@gmail.com>
2025-05-20 10:04:05 -0500

    styles NavBar active links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,33 @@
+'use client';
+
 import Link from "next/link";
 import styles from "./styles/Navbar.module.css";
 import {BiSolidHomeHeart, BiSolidCalendar} from "react-icons/bi";
 import {ImLeaf} from "react-icons/im";
 import {AiOutlinePlus} from "react-icons/ai";
+import { usePathname } from "next/navigation";
 
 export default function Navbar(){
+    const pathname = usePathname();
+
     return(
         <nav className={styles.nav}>
-            <Link href="/home"><BiSolidHomeHeart fontSize={'35px'}/></Link>
-            <Link href="/add"><AiOutlinePlus fontSize={'38px'}/></Link>
-            <Link href="/plants"><ImLeaf fontSize={'30px'}/></Link>
-            <Link href="/calendar"><BiSolidCalendar fontSize={'35px'}/></Link>            
+            <Link href="/home" className={pathname === '/home' ? styles.active : undefined}>
+                <BiSolidHomeHeart fontSize={'35px'} />
+                <span>Home</span>
+            </Link>
+            <Link href="/add" className={pathname === '/add' ? styles.active : undefined}>
+                <AiOutlinePlus fontSize={'38px'} />
+                <span>Agregar</span>
+            </Link>
+            <Link href="/plants" className={pathname === '/plants' ? styles.active : undefined}>
+                <ImLeaf fontSize={'30px'} />
+                <span>Mis plantas</span>
+            </Link>
+            <Link href="/calendar" className={pathname === '/calendar' ? styles.active : undefined}>
+                <BiSolidCalendar fontSize={'35px'} />
+                <span>Calendario</span>
+            </Link>            
         </nav>
-    )
+    );
 }
