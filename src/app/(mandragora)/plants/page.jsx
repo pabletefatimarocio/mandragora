b869930ddf33afff0f4c945a6ed394cfa0a8e3dd
pabletefatimarocio <pabletefatima9@gmail.com>
2025-05-27 17:49:15 -0500

    my plants searchbar

--- src/app/(mandragora)/plants/page.jsx
+++ src/app/(mandragora)/plants/page.jsx
@@ -1,14 +1,18 @@
-import Image from "next/image";
 import styles from "./MyPlantsPage.module.css";
 import CardsPlants from "@/components/CardsPlants";
-
+import { FaMagnifyingGlass } from "react-icons/fa6";
 
 export default function MyPlantsPage() {
-   return (
-    
+  return (
     <div className={styles.container}>
-      <CardsPlants type="myplants"/>
+      <div className={styles.title}>
+        <h1>Mis plantas</h1>
+      </div>
+      <div className={styles.searchbar}>
+        <input type="text" placeholder="Busca tu planta..."/>
+        <FaMagnifyingGlass className={styles.icon}/>
+      </div>
+      <CardsPlants type="myplants" />
     </div>
-    
   );
 }
