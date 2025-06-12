4750682efd03974dd2cce78af41eb3c187485034
Juan José Rodriguez <zoranbow@gmail.com>
2025-06-12 12:13:35 -0500

    /api/plants/details/:id GET endpoint

--- src/app/api/plants/details/[id]/route.js
+++ src/app/api/plants/details/[id]/route.js
@@ -1,22 +1,39 @@
-import { cardsPlants } from '@/utils/mock/plants';
-import { NextResponse } from "next/server"
+import { auth } from "@/lib/auth";
+import prisma from "@/lib/db";
+import { NextResponse } from "next/server";
 
 export async function GET(request, { params }) {
-  const { id } = await params;
+  try {
+    const { id } = await params;
 
-  console.log(id);
+    const session = await auth();
 
-  const plantDetails = cardsPlants.find((plant) => plant.id === JSON.parse(id));
+    const user_id = session.user.id;
 
-  if (!plantDetails) {
+    if (!user_id) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 403 });
+    }
+
+    const plantDetails = await prisma.plant.findUnique({
+      where: {
+        id,
+        user_id,
+      },
+    });
+
+    if (!plantDetails) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 403 });
+    }
+
+    return NextResponse.json(plantDetails, { status: 200 });
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(error.stack);
+    }
+
     return NextResponse.json(
-      { error: 'Plant not found' },
-      { status: 404 }
+      { error: error.message || "Internal server error" },
+      { status: 500 }
     );
   }
-
-  return NextResponse.json(
-    plantDetails,
-    { status: 200 }
-  );
 }
