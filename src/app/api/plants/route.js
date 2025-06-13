58a56725943b32186c2a869dd24b12b0d70f9923
Juan José Rodriguez <zoranbow@gmail.com>
2025-06-13 15:16:33 -0500

    C:/Program Files/Git/api/plants POST endpoint

--- src/app/api/plants/route.js
+++ src/app/api/plants/route.js
@@ -1,5 +1,4 @@
 import { NextResponse } from "next/server";
-import { cardsPlants } from "@/utils/mock/plants";
 import { auth } from "@/lib/auth";
 import prisma from "@/lib/db";
 
@@ -30,6 +29,70 @@ export async function GET() {
     });
 
     return NextResponse.json(plantsCards, { status: 200 });
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(error.stack);
+    }
+
+    return NextResponse.json(
+      { error: error.message || "Internal server error" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function POST(request) {
+  try {
+    const session = await auth();
+
+    const user_id = session.user.id;
+
+    if (!user_id) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 403 });
+    }
+
+    const {
+      name,
+      scientific,
+      img,
+      watering,
+      fertilization
+    } = await request.json();
+
+    const foundPlant = await prisma.plant.findUnique({
+      where: {
+        user_id,
+        name
+      },
+      select: {
+        id: true
+      }
+    });
+
+    if (foundPlant) {
+      return NextResponse.json(
+        { error: "Plant name already in use" },
+        { status: 400 }
+      );
+    }
+
+    const newPlant = await prisma.plant.create({
+      data: {
+        name,
+        scientific,
+        img,
+        watering,
+        fertilization
+      },
+      select: {
+        id: true
+      }
+    });
+
+    return NextResponse.json(
+      { message: "Plant successfully created", data: newPlant },
+      { status: 201 }
+    );
   } catch (error) {
     if (error instanceof Error) {
       console.error(error.stack);
