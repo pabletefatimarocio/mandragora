188023cb6ca2ba982acf4f840ae2ded09a36d678
pabletefatimarocio <pabletefatima9@gmail.com>
2025-06-24 17:58:00 -0500

    PlantDetails page

--- src/components/PlantDetails.jsx
+++ src/components/PlantDetails.jsx
@@ -1,21 +1,143 @@
-'use client';
+"use client";
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import styles from "./styles/PlantDetails.module.css";
+import Image from "next/image";
+import { PiPottedPlantFill } from "react-icons/pi";
+import { GiPowderBag } from "react-icons/gi";
+import { BsDropletFill } from "react-icons/bs";
+import { AiOutlinePlusCircle } from "react-icons/ai";
+import { PiPencilLight } from "react-icons/pi";
 
-export default function PlantDetails({ id }){
-  const [ plant, setPlant ] = useState({ name: '' });
+const plantInitialState = {
+  img: "",
+  name: "",
+  scientific: "",
+  watering: null,
+  waterings: [],
+  fertilization: null,
+  fertilizations: [],
+  location_type: "",
+  location_place: "",
+  tags: [],
+  notes: [],
+};
+
+export default function PlantDetails({ id }) {
+  const [plant, setPlant] = useState(plantInitialState);
 
   useEffect(() => {
     fetch(`/api/plants/details/${id}`)
-      .then(res => res.json())
-      .then(resJSON => setPlant(resJSON))
+      .then((res) => res.json())
+      .then((resJSON) => {
+        return setPlant(resJSON);
+      });
   }, []);
 
-  return(
+  if (plant.name === "") return <div>LOADING...</div>;
+
+  const lastWatering = new Date(
+    plant.waterings[plant.waterings.length - 1]
+  ).toLocaleDateString("es", { month: "long", day: "numeric" });
+
+  const lastFertilization = new Date(
+    plant.fertilizations[plant.fertilizations.length - 1]
+  ).toLocaleDateString("es", { month: "long", day: "numeric" });
+
+  return (
     <div className={styles.container}>
-      <h1>DETAILS</h1>
-      <h2>{plant.name}</h2>
+      <div className={styles.backgroundImg}>
+        <Image src={plant.img} alt="" fill priority />
+      </div>
+
+      <div className={styles.content}>
+        {/* INFO */}
+        <div className={styles.info}>
+          <PiPencilLight className={styles.pencilInfo} />
+          <div className={styles.name}>
+            <h1>{plant.name}</h1>
+            <h3>{plant.scientific}</h3>
+          </div>
+
+          <div className={styles.attributes}>
+            <div className={styles.attribute}>
+              <div className={styles.iconDrop}>
+                <BsDropletFill />
+              </div>
+              <div className={styles.text}>
+                <p>
+                  Riego cada <b>{plant.watering}</b> días
+                </p>
+                <p>
+                  Ultimo riego: <b>{lastWatering}</b>
+                </p>
+              </div>
+            </div>
+            <div className={styles.attribute}>
+              <div className={styles.iconBag}>
+                <GiPowderBag />
+              </div>
+              <div className={styles.text}>
+                <p>
+                  Fertilizante cada <b>{plant.fertilization}</b> días
+                </p>
+                <p>
+                  Última fertilización: <b>{lastFertilization}</b>
+                </p>
+              </div>
+            </div>
+            <div className={styles.attribute}>
+              <div className={styles.iconPlant}>
+                <PiPottedPlantFill />
+              </div>
+              <div className={styles.text}>
+                <p>{plant.location_type}</p>
+                <p>
+                  <b>{plant.location_place}</b>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* TAGS */}
+        <div className={styles.tags}>
+          {plant.tags.map((tag) => (
+            <div
+              key={tag.id}
+              className={styles.tag}
+              style={{ backgroundColor: tag.color }}
+            >
+              <p>{tag.name}</p>
+            </div>
+          ))}
+          <AiOutlinePlusCircle className={styles.plus} />
+        </div>
+        {/* NOTES */}
+        <div className={styles.notes}>
+          <div className={styles.titleNotes}>
+            <h1>NOTAS</h1>
+            <PiPencilLight className={styles.pencilNotes} />
+          </div>
+          <hr className={styles.hr} />
+          <div className={styles.notesContent}>
+            {plant.notes.map((note) => {
+              const date = new Date(note.created_at).toLocaleDateString("es", {
+                month: "long",
+                day: "numeric",
+                year: "numeric",
+                hour: "2-digit",
+                minute: "2-digit",
+              });
+              return (
+                <div key={note.id}>
+                  <p className={styles.noteDate}>{date}</p>
+                  <p>{note.content}</p>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
